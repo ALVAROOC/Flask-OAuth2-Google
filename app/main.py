f5fcf2686896d4dc6769662979b9bb4ca281aecc
Alvarooc <alvarooliverc@gmail.com>
2021-01-01 11:44:58 +0100

    OAuth Working

--- app/main.py
+++ app/main.py
@@ -10,8 +10,8 @@ app.secret_key = 'random secret'
 oauth = OAuth(app)
 google = oauth.register(
     name='google',
-    client_id='',
-    client_secret='',
+    client_id='223311974439-6948udd4tnd2jr60kbfv11g0knp9hsdc.apps.googleusercontent.com',
+    client_secret='UJw7PpiZcHKbG0_aOQvNO665',
     access_token_url='https://accounts.google.com/o/oauth2/token',
     acess_token_params=None,
     authorize_url='https://accounts.google.com/o/oauth2/auth',
@@ -30,7 +30,7 @@ def home():
 def login():
     google = oauth.create_client('google') 
     redirect_url = url_for('authorize', _external=True)
-    return oauth.twitter.authorize_redirect(redirect_url)
+    return google.authorize_redirect(redirect_url)
 
 
 @app.route("/authorize")
@@ -40,9 +40,14 @@ def authorize():
     resp = google.get('userinfo')
     user_info = resp.json()
     # Do something with the token and profile
+    session['email'] = user_info['email']
     return redirect("/")
 
-
+@app.route('/logout')
+def logout():
+    for key in list(session.keys()):
+        session.pop(key)
+    return redirect("/")
 
 
 if __name__ == '__main__':
